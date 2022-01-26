a6ba3aa51f45ff95173db2a7a698f1f8d1cf8894
repetitioestmaterstudiorum <thotan@ik.me>
2022-01-26 21:36:30 +0100

    w5 speller solved

--- week-5/speller/dictionary.c
+++ week-5/speller/dictionary.c
@@ -2,6 +2,11 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -13,43 +18,100 @@ typedef struct node
 }
 node;
 
-// TODO: Choose number of buckets in hash table
-const unsigned int N = 1;
+// Number of buckets in hash table
+const unsigned int N = 26; // 26 letters in the english alphabet
 
 // Hash table
 node *table[N];
+int words = 0;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
-    return false;
+    bool found = false;
+    for (node *tmp = table[hash(word)]; tmp != NULL; tmp = tmp->next)
+    {
+        if (strcasecmp(tmp->word, word) == 0)
+        {
+            found = true;
+            break; // no need to look further
+        }
+    }
+    
+    return found;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    // return toupper(word[0]) - 'A';
+    return tolower(word[0]) - 97; // a in ascii = 97. a/A => 0, b/B => 1 ... until z/Z => 25
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // store dictionary in a hash table (array of linked lists)
+    FILE *dict_file = fopen(dictionary, "r");
+    if (dict_file == NULL)
+    {
+        fclose(dict_file);
+        return false;
+    }
+
+    // char *word = malloc(sizeof(LENGTH));
+    char word[LENGTH];
+    while (fscanf(dict_file, "%s", word) != EOF)
+    {
+        // printf("> %s\n", word);
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+        {
+            free(new_node);
+            return false;
+        }
+        strcpy(new_node->word, word); // add word to new node
+        new_node->next = NULL; // initially, set new node's next value to NULL
+
+        unsigned int h = hash(word); // get index where to insert new node into table
+
+        // insert node into table
+        if (table[h] == NULL) // no linked list yet in table index of that word's hash
+        {
+            table[h] = new_node;
+        }
+        else // list exists at that index, insert new word at the beginning of the linked list 
+        {
+            new_node->next = table[h];
+            table[h] = new_node;
+        }
+
+        words += 1;
+    }
+
+    fclose(dict_file);
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return words;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        while (table[i] != NULL)
+        {
+            node *tmp = table[i]->next; // find 2nd node
+            free(table[i]); // free first node
+            table[i] = tmp; // assign list node 2, repeat cycle
+        }
+    }
+
+    return true;
 }
--- week-5/speller/speller.c
+++ week-5/speller/speller.c
@@ -198,7 +198,7 @@ double calculate(const struct rusage *b, const struct rusage *a)
 }
 
 /*
-compile and run in docker: clang -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow -c -o speller.o speller.c && clang -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow -c -o dictionary.o dictionary.c && clang -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow -o speller.out speller.o dictionary.o -lm && rm -f *.o && ./speller.out
+compile and run in docker: clang -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow -c -o speller.o speller.c && clang -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow -c -o dictionary.o dictionary.c && clang -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow -o speller.out speller.o dictionary.o -lm && rm -f *.o && ./speller.out ./dictionaries/large aachen
 check: check50 cs50/problems/2022/x/speller
 submit: submit50 cs50/problems/2022/x/speller
 */
